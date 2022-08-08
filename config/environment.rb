811369d1855f2518fcbdb0fa0c35fe5515bef69c
Alexander Illarionov <alex@heimstudio.ru>
2022-08-08 12:10:21 +0200

    rollbar issue

--- config/environment.rb
+++ config/environment.rb
@@ -5,20 +5,20 @@ require_relative 'application'
 require_relative 'rollbar'
 
 # Initialize the Rails application.
-# Rails.application.initialize!
+Rails.application.initialize!
 
-notify = lambda do |e|
-  Rollbar.with_config(use_async: false) do
-    Rollbar.error(e)
-  end
-rescue StandardError
-  Rails.logger.error 'Synchronous Rollbar notification failed.  Sending async to preserve info'
-  Rollbar.error(e)
-end
+# notify = lambda do |e|
+#   Rollbar.with_config(use_async: false) do
+#     Rollbar.error(e)
+#   end
+# rescue StandardError
+#   Rails.logger.error 'Synchronous Rollbar notification failed.  Sending async to preserve info'
+#   Rollbar.error(e)
+# end
 
-begin
-  Rails.application.initialize!
-rescue Exception => e
-  notify.call(e)
-  raise
-end
+# begin
+#   Rails.application.initialize!
+# rescue Exception => e
+#   notify.call(e)
+#   raise
+# end
